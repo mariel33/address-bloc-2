6b6297af7fe26ca88e25f07637ca2109b83e94b7
Mariel <mariel.capulli@gmail.com>
2018-06-09 15:25:46 -0400

    testing assignment

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -6,14 +6,18 @@ describe("MenuController", () => {
         this.menu = new MenuController();
     })
     describe("#getContactCount()", () => {
-        
+
         it("should return 0 when no contacts are in the book", () => {
             expect(this.menu.getContactCount()).toBe(0);
         });
 
-        it("should return 1 when there is exactly one contact int he book", () => {
+        it("should return 1 when there is exactly one contact in the book", () => {
             this.menu.contacts.push("Bob");
             expect(this.menu.getContactCount()).toBe(1);
+        });
+
+        it("should return a data value that is a  string", () => {
+            expect(this.menu.remindMe()).toEqual("Learning is a life-long pursuit");
         });
     });
 });
