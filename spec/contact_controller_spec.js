340cef8021672495c0c6f0288b9b346f6ea55738
Mariel <mariel.capulli@gmail.com>
2018-06-10 13:42:03 -0400

    contacts checkpoint

---
+++ spec/contact_controller_spec.js
@@ -0,0 +1,34 @@
+const ContactController = require("../controllers/ContactController");
+const sequelize = require("../db/models/index").sequelize;
+
+describe("ContactController", () => {
+    it("should be defined", () => {
+        expect(ContactController).toBeDefined();
+      });
+
+    beforeEach((done) => {
+        this.book = new ContactController();
+
+        sequelize.sync({ fore: true }).then((res) => {
+            done();
+        })
+            .catch((err) => {
+                done();
+            });
+    });
+
+    describe("#addContact()", () => {
+        it("should add a single contact into the book", (done) => {
+            this.book.addContact("Alice", "001-101-1010")
+            .then((contact) => {
+                expect(contact.name).toBe("Alice");
+                expect(contact.phone).toBe("001-101-1010");
+                done();
+            })
+            .catch((err) => {
+                done();
+            })
+
+        });
+    });
+})
